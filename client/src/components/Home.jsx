6331ba99fff51a0ee84015e3e18e460b6ca5ac25
Ansh Agarwal <122566057+Ansh642@users.noreply.github.com>
2023-11-27 11:02:18 +0530

    Footer and image slider added

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,12 +1,34 @@
 import React from 'react'
 import Navbar from './Navbar'
-import { Link } from 'react-router-dom'
+import Footer from './Footer';
+
+import ImageSlider from './ImageSlider'
+import image1 from '../images/a.jpg'
+import image2 from '../images/b.jpg'
+import image3 from '../images/c.jpg'
+import image4 from '../images/d.jpg'
+
+const images = [
+  image3,
+  image1,
+  image4
+  // Add more image URLs here...
+];
+
 
 const Home = () => {
   return (
-    <div>
+  <> 
+
+      <Navbar/>
+
+      <div className="flex justify-center items-center min-h-screen bg-gray-100">
+      <ImageSlider images={images} />
+
       
     </div>
+    <Footer/>
+    </>
   )
 }
 
---
+++ client/src/components/ImageSlider.jsx
@@ -0,0 +1,40 @@
+// ImageSlider.js
+import React, { useState } from 'react';
+
+const ImageSlider = ({ images }) => {
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  const goToPrevSlide = () => {
+    const index = (currentImageIndex - 1 + images.length) % images.length;
+    setCurrentImageIndex(index);
+  };
+
+  const goToNextSlide = () => {
+    const index = (currentImageIndex + 1) % images.length;
+    setCurrentImageIndex(index);
+  };
+ 
+  return (
+    <div className="relative w-full h-96 -mt-48">
+      <img
+        src={images[currentImageIndex]}
+        alt={`Slide ${currentImageIndex + 1}`}
+        className="object-cover w-full h-full"
+      />
+      <button
+        className="absolute left-0 top-1/2 transform -translate-y-1/2 bg-white bg-opacity-50 px-2 py-1 rounded"
+        onClick={goToPrevSlide}
+      >
+        &lt;
+      </button>
+      <button
+        className="absolute right-0 top-1/2 transform -translate-y-1/2 bg-white bg-opacity-50 px-2 py-1 rounded"
+        onClick={goToNextSlide}
+      >
+        &gt;
+      </button>
+    </div>
+  );
+};
+
+export default ImageSlider;
